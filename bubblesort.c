3a5d00fdee9fb3f5252f05eee46de4e4ac7761a7
ThresySebastian <167005336+ThresySebastian@users.noreply.github.com>
2024-08-22 20:21:45 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,32 +1,52 @@
 #include <stdio.h>
-void main()
-{
-    int n,i, j, temp;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-    int a[n];
-    printf("Enter %d elements:\n", n);
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
-    for (i = 0; i < n - 1; i++)
-    {
-        for (j = 0; j < n - i - 1; j++)
-        {
-            if (a[j] > a[j + 1])
-            {
-                temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
-            }
-        }
-    }
-    printf("Sorted array: \n");
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
+int main() {
+int n, i, j, temp, time = 0;
+
+printf("Enter the number of elements: ");
+time++;
+scanf("%d", &n);
+time++;
+int a[n];
+time++;
+
+printf("Enter %d elements:\n", n);
+time++;
+for (i = 0; i < n; i++) {
+time++;
+scanf("%d", &a[i]);
+time++;
 }
+
+for (i = 0; i < n - 1; i++) {
+time++;
+for (j = 0; j < n - i - 1; j++) {
+time++;
+if (a[j] > a[j + 1]) {
+time++;
+temp = a[j];
+time++;
+a[j] = a[j + 1];
+time++;
+a[j + 1] = temp;
+time++;
+}
+}
+}
+
+printf("Sorted array: \n");
+time++;
+for (i = 0; i < n; i++) {
+time++;
+printf("%d ", a[i]);
+time++;
+}
+
+printf("\nSpace Complexity: %d ",(4 * 4) + (4 * n) );
+time++;
+time++;
+printf("\nTime Complexity: %d\n", time);
+
+return 0;
+}
+
 
